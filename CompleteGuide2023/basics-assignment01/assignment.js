81bf98311a2260e43d4b0fba3b0f912db4511960
DESKTOP-6FBGBC6\GDJEva <gdjevans@hotmail.com>
2022-12-14 23:40:35 +0000

    Start basics section - variables, numbers, string.

---
+++ CompleteGuide2023/basics-assignment01/assignment.js
@@ -0,0 +1,15 @@
+const userInput = 5;
+
+let result = 18;
+
+result = result + userInput;
+
+result = result - userInput;
+
+result = result * userInput;
+ 
+result = result / userInput;
+
+alert('User Input was - ' + userInput);
+
+alert('Result is - ' + result);
