90ffa67c2464d4a10b3e69b96ba8c4cf2fdf7f93
DESKTOP-6FBGBC6\GDJEva <gdjevans@hotmail.com>
2023-01-05 00:47:39 +0000

    JS Concepts - Vatiables, Statements and Loops

---
+++ Snake/concepts/script.js
@@ -0,0 +1,97 @@
+let fruit = 'Apple';
+
+let num = 5;
+
+let num1 = 1.5;
+
+let isHere = false;
+
+
+// Conditional statements
+// if else, esle if, switch
+
+// If condition is true
+//     Do this
+// else
+//     Do this
+
+// if(condition) {
+//     lines of code
+// }
+// else {
+//     lines of code
+// }
+
+// switch(variable or condition) {
+//     case "first": statements/lines of ConvolverNode;
+//                     break;
+//     case "second" : statements;
+//                     break;
+//     default: statements;   
+// }
+
+// let num20 = 5;
+
+// switch(num20) {
+//     case 1: alert("The number is 1");
+//     break;
+//     case 3: alert("The number is 3");
+//     break;
+//     case 5: alert("The number is 5");
+//     break;
+//     case 7: alert("The number is 7");
+//     break;
+//     default: alert("The number is unknown");
+// }
+
+// Looping statements
+
+// for(initialisation; condition; increment) {
+//     statement / lines of code
+// }
+
+// for(let i = 1; i<=5; i++){
+//     document.write(i);
+// }
+
+//Arrays
+
+// let number = new Array();
+// number = [1, 2, 3, 4, 5];
+// alert(number[0]);
+// number [5] = 6;
+// //[1, 2, 3, 4, 5, 6];
+// number[4] = 7;
+// //[1, 2, 3, 4, 7, 6];
+
+// let number1 = ["one", "two", "three"];
+// alert(number1[1]);
+
+//2 dimensional arrays
+//arrays within arrays
+
+// var numValues = [[1,2,3], [4,5,6], [7, 8, 9]];
+// alert(numValues[0][1]);
+
+//Function
+//function definition
+
+// function functionName() {
+
+// }
+
+// //function call
+
+// functionName();
+
+
+// let numFirst = 5;
+// let numSecond = 6;
+
+// function sumNum(n1, n2) {
+//     let sum = n1 + n2;
+//     return sum;
+// }
+
+// let valSum = sumNum(numFirst, numSecond);
+// alert("the sum of the number " + valSum)
