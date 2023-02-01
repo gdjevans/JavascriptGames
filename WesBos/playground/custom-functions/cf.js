9e8cb0eeba4fe21cd8b8e8beed7c1b6408df1efa
DESKTOP-6FBGBC6\GDJEva <gdjevans@hotmail.com>
2023-02-01 22:53:18 +0000

    Functions - Parameters

--- WesBos/playground/custom-functions/cf.js
+++ WesBos/playground/custom-functions/cf.js
@@ -1,15 +1,41 @@
 // function definition
-function calculateBill() {
+
+function calculateBill(billAmount, taxRate = 0.13, tipRate = 0.15) {
   // this is the function body
   console.log('Running Calculate Bill!!');
-  const total = 100 * 1.13;
-  console.log(total);
+  const total = billAmount + (billAmount + taxRate) + (billAmount + tipRate);
   return total;
 }
 
-// functionm call
+// function call
 
-const myTotal = calculateBill();
-console.log(`Your total is ${myTotal}`);
+const myTotal = calculateBill(100, 0.13);
 
-console.log(`Your total is $${calculateBill()}`);
+console.log(myTotal);
+
+// console.log(`Your total is ${myTotal}`);
+
+// console.log(`Your total is $${calculateBill()}`);
+
+function sayHiTo(firstName) {
+  return `Hello ${firstName}`;
+}
+
+const greeting = sayHiTo('Glyn');
+
+console.log(greeting);
+
+const myTotal3 = calculateBill(20 + 20 + 30 + 20, 0.15);
+
+function doctorize(name) {
+  return `Dr. ${name}`;
+}
+
+function yell(name = 'silly goose') {
+  return `HEY ${name.toUpperCase()}`;
+}
+
+yell(doctorize('Glyn'));
+
+const myBill4 = calculateBill(100, undefined, 0.2);
+console.log(myBill4);
