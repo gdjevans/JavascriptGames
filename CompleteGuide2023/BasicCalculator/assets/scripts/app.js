6b54cd49e3b361e5f5bf5d02ef8e795c77a3ff85
DESKTOP-6FBGBC6\GDJEva <gdjevans@hotmail.com>
2023-03-22 22:17:06 +0000

    Basics - Functions

--- CompleteGuide2023/BasicCalculator/assets/scripts/app.js
+++ CompleteGuide2023/BasicCalculator/assets/scripts/app.js
@@ -1,8 +1,15 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-currentResult = (currentResult + 10) * 3 / 2 -1;
+function add() {
+    currentResult = currentResult + userInput.value;
+    outputResult(currentResult, '');
+}
 
-let calculationDescription = `(${defaultResult} + 10) * 3 / 2 -1`;
+addBtn.addEventListener('click', add);
 
-outputResult(currentResult, calculationDescription);
+//currentResult = (currentResult + 10) * 3 / 2 -1;
+
+//let calculationDescription = `(${defaultResult} + 10) * 3 / 2 -1`;
+
+
