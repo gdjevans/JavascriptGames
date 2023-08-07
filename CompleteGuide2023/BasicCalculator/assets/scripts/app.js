6d413e52b9b60173f9cd77f303cb7dccedf1336b
gdjevans@hotmail.com <gdjevans@hotmail.com>
2023-08-07 09:04:58 +0100

    Functions

--- CompleteGuide2023/BasicCalculator/assets/scripts/app.js
+++ CompleteGuide2023/BasicCalculator/assets/scripts/app.js
@@ -1,15 +1,52 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
+// Gets input from the user field.
+function getUserNumberInput() {
+    return parseInt(userInput.value);
+}
+
+//Generates and writes calculation log step.
+function createandWriteOutput(operator, resultBeforeCalc, calcNumber) {
+    const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+    outputResult(currentResult, calcDescription); // from vendor file.
+}
+
 function add() {
-    currentResult = currentResult + parseInt(userInput.value);
-    outputResult(currentResult, '');
+    const enteredNumber = getUserNumberInput();
+    const initalResult = currentResult;
+    currentResult += enteredNumber;
+    createandWriteOutput('+', initalResult, enteredNumber);
+}
+
+function subtract() {
+    const enteredNumber = getUserNumberInput();
+    const initalResult = currentResult;
+    currentResult -= enteredNumber;
+    createandWriteOutput('-', initalResult, enteredNumber);
+}
+
+function multiply() {
+    const enteredNumber = getUserNumberInput();
+    const initalResult = currentResult;
+    currentResult *= enteredNumber;
+    createandWriteOutput('*', initalResult, enteredNumber);
+}
+
+function divide() {
+    const enteredNumber = getUserNumberInput();
+    const initalResult = currentResult;
+    currentResult /= enteredNumber;
+    createandWriteOutput('/', initalResult, enteredNumber);
 }
 
 addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
+
 
 //currentResult = (currentResult + 10) * 3 / 2 -1;
-
 //let calculationDescription = `(${defaultResult} + 10) * 3 / 2 -1`;
 
 
