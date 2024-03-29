ace1d357ea8709e6eeebd8560bfeccb14ddc0ad8
DESKTOP-0QATB9U\GDJEvans <gdjevans@hotmail.com>
2024-03-29 12:11:14 +0000

    Styling Fixes for NASA App.

--- ZTM20Projects/NASA/script.js
+++ ZTM20Projects/NASA/script.js
@@ -1,6 +1,6 @@
 const resultsNav = document.getElementById('resultsNav');
 const favoritesNav = document.getElementById('favoritesNav');
-const imagesContainer = document.querySelector('.image-container');
+const imagesContainer = document.querySelector('.images-container');
 const saveConfirmed = document.querySelector('.save-confirmed');
 const loader = document.querySelector('loader');
 
